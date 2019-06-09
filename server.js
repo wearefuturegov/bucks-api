1c6a54aa769572d198187be1318fe2516bf71d06
Jaye <joshua.hackett93@gmail.com>
2019-06-09 21:34:21 +0100

    stub out basic api endpoints

--- server.js
+++ server.js
@@ -2,27 +2,41 @@ const express = require('express')
 const next = require('next')
 const mongoose = require('mongoose')
 
+const Service = require('./models/Service')
+
 require('dotenv').config()
-
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}, (err)=>{
-  if(err) return console.log(err)
-  console.log("> DB connection opened")
-})
-
-
-
-
-
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 const port = process.env.PORT || 3000
 
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}, (err)=>{
+  if(err) return console.log(err)
+  console.log("> DB connection opened 🎉")
+})
+
 app
   .prepare()
   .then(() => {
     const server = express()
+
+    
+    // Basic list endpoint
+    server.get("/services", (req, res)=>{
+      Service.find().limit(10)
+      .exec((err, services)=>{
+        res.json(services)
+      })
+    })
+
+    // Basic single endpoint
+    server.get("/services/:id", (req, res)=>{
+      Service.findOne({assetId: req.params.id}, (err, service)=>{
+        res.json(service)
+      })
+    })
+
 
     server.get('*', (req, res) => {
       return handle(req, res)
@@ -30,7 +44,7 @@ app
 
     server.listen(port, err => {
       if (err) throw err
-      console.log(`> Ready on port ${port}`)
+      console.log(`> Ready on port ${port} 🎁`)
     })
   })
   .catch(ex => {
