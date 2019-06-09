d302e9bd44a1da1f499f5e313a6ae340502b3b86
Jaye <joshua.hackett93@gmail.com>
2019-06-09 23:04:24 +0100

    change db config var name to match heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 const port = process.env.PORT || 3000
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true}, (err)=>{
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}, (err)=>{
   if(err) return console.log(err)
   console.log("> DB connection opened 🎉")
 })
