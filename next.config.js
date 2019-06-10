c6159ea5085bf2c7d10725f8396d8d1cd414957e
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-06-10 22:52:06 +0100

    set up postcss

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 const withImages = require('next-images')
 
-module.exports = withImages(withSass({
+module.exports = withImages(withSass(withCSS({
   sassLoaderOptions: {
     includePaths: []
   }
-}))
+})))
