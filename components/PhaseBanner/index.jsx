3f542f5d31f911fa13ecb073c91e333a9e34c953
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-06-11 15:43:02 +0100

    nicer phase banner layout

--- components/PhaseBanner/index.jsx
+++ components/PhaseBanner/index.jsx
@@ -4,8 +4,8 @@ import './style.scss'
 
 const PhaseBanner = ({href}) =>
     <section className="phase-banner">
-        <div className="container phase-banner__inner">
-            <p><strong className="phase-banner__tag">Beta</strong><span className="phase-banner__inner-text">This is a new service - <a href={href}>your feedback</a> will help us improve it.</span></p>
+        <div className="container">
+            <p className="phase-banner__inner"><strong className="phase-banner__tag">Beta</strong><span className="phase-banner__inner-text">This is a new service - <a href={href}>your feedback</a> will help us improve it.</span></p>
         </div>
     </section>
 
