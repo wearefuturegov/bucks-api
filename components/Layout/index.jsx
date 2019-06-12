2346755fe8399761a88e1e88393664ea39e4283f
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-06-12 09:43:18 +0100

    put header in layout

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Head from 'next/head'
+import Header from '../Header'
 import PhaseBanner from '../PhaseBanner'
 import IsPageUseful from '../IsPageUseful'
 import FullWidthSearch from '../FullWidthSearchPanel'
@@ -13,6 +14,24 @@ const Layout = ({children})=>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700&display=swap" rel="stylesheet"/> 
     </Head>
     <PhaseBanner href="#"/>
+    <Header menuItems={[
+        {
+            href: "#",
+            text: "Information and advice"
+        },
+        {
+            href: "/recommendations",
+            text: "All services"
+        },
+        {
+            href: "#",
+            text: "Log in"
+        },
+        {
+            href: "#",
+            text: "Sign up"
+        },
+    ]}/>
     {children}
     <IsPageUseful/>
     <FullWidthSearch
