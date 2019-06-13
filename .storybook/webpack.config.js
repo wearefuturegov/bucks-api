3fe4c4ab41ec3007c9adf63fc99eb2d0f43528ca
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-06-13 09:37:48 +0100

    started on getting storybook-source working

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,19 +4,27 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-                include: path.resolve(__dirname, '../'),
+              test: /\.(scss|css)$/,
+              use: ['style-loader', 'css-loader', 'sass-loader'],
+              include: path.resolve(__dirname, '../'),
             },
             {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                  {
-                    loader: 'file-loader',
-                    options: {},
-                  },
-                ],
-              },
+              test: /\.(png|jpe?g|gif|svg)$/,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {},
+                },
+              ],
+            },
+            {
+              test: /\.stories\.js?$/,
+              loaders: [require.resolve('@storybook/addon-storysource/loader')],
+              enforce: 'pre',
+            },
         ],
     },
 };
+
+
+
