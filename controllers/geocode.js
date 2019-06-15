be71f5c660bb6fde2f565cbf81213990e5d6da3a
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-06-15 11:24:12 +0100

    geocoder is region-aware

--- controllers/geocode.js
+++ controllers/geocode.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fetch = require('isomorphic-unfetch')
 
 module.exports = async (req, res) => {
-    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.location}&key=${process.env.GOOGLE_API_KEY}`)
+    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.location}, UK&key=${process.env.GOOGLE_API_KEY}`)
     const data = await response.json()
     res.json(data)
 }
