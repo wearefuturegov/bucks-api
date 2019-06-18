5d206d24f1173ca133463b254a3b83cb0af9db39
Jaye <joshua.hackett93@gmail.com>
2019-06-18 20:00:01 +0100

    fix chrome lighthouse bug

--- components/SearchForm/index.jsx
+++ components/SearchForm/index.jsx
@@ -5,10 +5,8 @@ import './style.scss'
 const SearchForm = ({large}) =>
     <form className={(large)? "search search--large" : "search"} action="https://www.careadvicebuckinghamshire.org/s4s/WhereILive/Council?pageId=2239" method="get">
         <label className="visually-hidden" htmlFor={(large)? "query-large" : "query"}>Search query</label>
-        
-        <input type="hidden" name="pageId" value="2239"/>
-        
         <input className="search__input" type="search" id={(large)? "query-large" : "query"} name="Search" placeholder="Search for advice" required></input>
+        <input type="hidden" name="pageId" value="2239"/>
         <button className="search__submit" type="submit">
             <img className="search__icon" src={icon} alt="Submit search"/>
         </button>
