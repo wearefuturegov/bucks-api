618c5a9c8537432dc7bf884ddd7e236b0268167a
Jaye <joshua.hackett93@gmail.com>
2019-06-18 19:49:01 +0100

    added ie polyfills for .find() and .includes()

--- next.config.js
+++ next.config.js
@@ -8,6 +8,22 @@ module.exports = withImages(
       sassLoaderOptions: {
         includePaths: []
       },
+      webpack: function (cfg) {
+        const originalEntry = cfg.entry
+        cfg.entry = async () => {
+          const entries = await originalEntry()
+          if (
+            entries['main.js'] &&
+            !entries['main.js'].includes('./lib/polyfills.js')
+          ) {
+            entries['main.js'].unshift('./lib/polyfills.js')
+          }
+          return entries
+        }
+        return cfg
+      }
     })
   )
 )
+
+
