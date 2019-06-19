f56d6149ec9c8d3fd04aa18bc22d63c6e5901711
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-06-19 15:53:33 +0100

    add no results message to service grid

--- components/ServicesGrid/index.jsx
+++ components/ServicesGrid/index.jsx
@@ -3,22 +3,32 @@ import PropTypes from 'prop-types'
 import ServiceCard from '../ServiceCard'
 import './style.scss'
 
+
+
+
 const ServicesGrid = ({services}) =>
     <>
         <h2 className="recommendations__section-title">Services near you</h2>
-        <ol className="services-grid">
-            {services.map((service, i)=>
-                <ServiceCard
-                    key={i}
-                    assetId={service.assetId}
-                    category={service.category}
-                    title={service.name || service.parentOrganisation}
-                    description={service.description}
-                    distance={service.distance}
-                    parentOrganisation={service.parentOrganisation}
-                    />
-            )}
-        </ol>
+        {services.length > 0 ? 
+            <ol className="services-grid">
+                {services.map((service, i)=>
+                    <ServiceCard
+                        key={i}
+                        assetId={service.assetId}
+                        category={service.category}
+                        title={service.name || service.parentOrganisation}
+                        description={service.description}
+                        distance={service.distance}
+                        parentOrganisation={service.parentOrganisation}
+                        />
+                )}
+            </ol>
+        : 
+            <div className="services-grid__no-results">
+                <h3>No results</h3>
+                <p>Improve your search results by removing filters, double-checking your spelling or searching for something less specific.</p>
+            </div>
+        }
     </>
 
 ServicesGrid.propTypes = {
