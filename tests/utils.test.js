e57a634ce93ab681e672301643bfba4a0a3ad833
Jaye <joshua.hackett93@gmail.com>
2019-06-25 11:04:54 +0100

    added a util function to prettify lists

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,4 +1,4 @@
-import {prettyDays, truncate, prettyDistance} from "../lib/utils"
+import {prettyDays, truncate, prettyDistance, prettyList} from "../lib/utils"
 
 describe("truncate", () => {
     it("should cope with one day", () => {
@@ -8,15 +8,15 @@ describe("truncate", () => {
         expect(prettyDays(["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"])).toBe("Available every day")
     })
     it("should produce falsy output if given empty input", () => {
-        expect(prettyDays([])).toBe(undefined)
+        expect(prettyDays([])).toBeFalsy()
     })
 })
 
 it("truncate", () => {
     expect(truncate("test text", 10)).toBe("test text")
     expect(truncate("test text test", 2)).toBe("test text...")
-    expect(truncate("test text test", 1)).toBe(undefined)
-    expect(truncate()).toBe(undefined)
+    expect(truncate("test text test", 1)).toBeFalsy()
+    expect(truncate()).toBeFalsy()
 })
 
 it("pretty distance", () => {
@@ -24,5 +24,11 @@ it("pretty distance", () => {
     expect(prettyDistance(1)).toBe("About a mile away")
     expect(prettyDistance(1.5)).toBe("About 2 miles away")
     expect(prettyDistance(1500)).toBe("About 1500 miles away")
-    expect(prettyDistance()).toBe(undefined)
+    expect(prettyDistance()).toBeFalsy()
+})
+
+it("pretty list", () => {
+    expect(prettyList()).toBeFalsy()
+    expect(prettyList([])).toEqual(expect.arrayContaining([]))
+    expect(prettyList(["el", "el2"])).toEqual(expect.arrayContaining(["El", "El2"]))
 })
