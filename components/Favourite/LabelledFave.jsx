f509c0210c56ae1ae63644ac61bc9a1cb1387b89
Jaye <joshua.hackett93@gmail.com>
2019-07-08 15:23:35 +0100

    tidy up fave icon design

--- components/Favourite/LabelledFave.jsx
+++ components/Favourite/LabelledFave.jsx
@@ -8,20 +8,23 @@ const Favourite = ({service, favourited, fave, unfave}) =>
         {favourited ?
             <button onClick={()=>{
                 unfave(service.assetId)
-            }} className="favourite-button--unlabelled">
+            }} className="favourite-button--labelled">
                 <div className="favourite-button__icon">
                     <img src={heartFilled} alt="Remove from saved services"/>
                 </div>
+                Remove from saved services
             </button>
             :
             <button onClick={()=>{
                 fave(service)
-            }} className="favourite-button--unlabelled">
+            }} className="favourite-button--labelled">
                 <div className="favourite-button__icon">
                     <img src={heart} alt="Save for later"/>
                 </div>
+                Save for later
             </button>
         }
     </>
+
 
 export default Favourite
