21e3ba1cbfd817d7745f31efdc8f124f48e914fa
Jaye <joshua.hackett93@gmail.com>
2019-07-12 14:35:58 +0100

    don't hardcode host

--- pages/_document.js
+++ pages/_document.js
@@ -7,18 +7,19 @@ class MyDocument extends Document {
     }
 
     render() {
+        
+        const host = process.env.HOST || "http://bucks-care-staging.herokuapp.com"
+
         return (
             <Html lang="en">
                 <Head>
                     <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
-
                     <meta name="twitter:card" content="summary"></meta>
                     <meta name="twitter:username" content="@buckscc"></meta>
                     <meta property="og:url" content="http://buckscc.gov.uk" />
                     <meta property="og:title" content="Care and support for adults, their families and carers" />
                     <meta property="og:description" content="Helping you find the right information and support in Buckinghamshire." />
-                    <meta property="og:image" content="http://bucks-care-staging.herokuapp.com/static/share-image.jpg" />
-
+                    <meta property="og:image" content={`${host}/static/share-image.jpg`} />
                 </Head>
                 <body>
                     <Main />
