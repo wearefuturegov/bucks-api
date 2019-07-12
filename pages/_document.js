8857bd06c3af6586fcfdbfd8ebcd14339ec7aca5
Jaye <joshua.hackett93@gmail.com>
2019-07-12 14:03:04 +0100

    added favicon

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
