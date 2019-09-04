aa45c24c74e2e2f00255fe81848869d4436e7a6d
Jaye <joshua.hackett93@gmail.com>
2019-09-04 19:40:26 +0100

    location filter display changes

---
+++ components/AutocompletePlacesInput/index.jsx
@@ -0,0 +1,53 @@
+import React, {useRef, useEffect} from "react"
+import styled from "styled-components"
+import theme from "../_theme"
+
+const Input = styled.input`
+    margin-top: 20px;
+    font-size: 1em;
+    border: 2px solid ${theme.darkText};
+    padding: 10px;
+    border-radius: 2px;
+    display: block;
+    width: 100%;
+    &:focus{
+        outline: none;
+        box-shadow: 0 0 0 3px ${theme.focus};
+    }
+`
+
+const LocationQuestion = ({
+    
+    setLatLng
+}) => {
+
+    const inputRef = useRef(false)
+
+    let autocomplete = null
+
+    useEffect(() => {
+        autocomplete = new window.google.maps.places.Autocomplete(inputRef.current, { 
+            types: ["geocode"]
+        })
+        autocomplete.setComponentRestrictions({"country": ["gb"]})
+        autocomplete.addListener("place_changed", handlePlaceChanged)
+    }, [])
+
+    const handlePlaceChanged = () => {
+        const place = autocomplete.getPlace()
+        const lat = place.geometry.location.lat()
+        const lng = place.geometry.location.lng()
+        setLatLng([lat, lng])
+    }
+
+    return(
+            <Input 
+                ref={inputRef}
+                id="autocomplete"
+                name="location" 
+                required
+            />
+    )
+}
+
+export default LocationQuestion
--- components/Filters/LocationFilter.jsx
+++ components/Filters/LocationFilter.jsx
@@ -1,5 +1,36 @@
 import React, { useState, useEffect } from "react"
 import { Dialog } from "@reach/dialog"
+import styled from "styled-components"
+import theme from "../_theme"
+import arrow from "./arrow-down.svg"
+
+const OpenerButton = styled.button`
+    background: none;
+    border: none;
+    padding: 5px 5px 10px 5px;
+    font-size: 1em;
+    color: ${theme.darkText};
+    cursor: pointer;
+    &:focus{
+        outline: none;
+        background: ${theme.focus};               
+    }
+    &:after{
+        content: "";
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-image: url(${arrow});
+        margin-left: 7px;
+    }
+`
+
+const StyledDialog = styled(Dialog)`
+    max-width: 600px;
+`
 
 const LocationFilter = () => {
 
@@ -7,19 +38,20 @@ const LocationFilter = () => {
 
     return (
         <>
-            <button 
+            <OpenerButton 
                 onClick={() => {toggleDialog(true)}}
                 className="test-1"
             >
                 Location
-            </button>
+            </OpenerButton>
 
-            <Dialog
+            <StyledDialog
                 isOpen={dialogOpen}
                 className="test-2"
+                onDismiss={()=>toggleDialog(false)}
             >
                 blah
-            </Dialog>
+            </StyledDialog>
             
         </>
     )
