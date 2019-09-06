ba992bc428c33a9f28bdeadfb5532552bad88f89
Jaye <joshua.hackett93@gmail.com>
2019-09-06 18:51:02 +0100

    reusable filter component working

--- components/Filters/Filter.jsx
+++ components/Filters/Filter.jsx
@@ -1,30 +1,194 @@
-import React from "react"
+import React, { useState, useEffect }  from "react"
+import Checkbox from "../Checkbox"
+import Router from "next/router"
+import styled from "styled-components"
+import theme from "../_theme"
 import { Dialog } from "@reach/dialog"
-import {} from "./utils"
+import arrow from "./arrow-down.svg"
 import cross from "./cross.svg"
 
+const OpenButton = styled.button`
+    background: none;
+    border: none;
+    padding: 5px;
+    margin-bottom: 10px;
+    font-size: 1em;
+    color: ${theme.darkText};
+    cursor: pointer;
+    margin-right: 10px;
+    font-weight: ${(props) => props.active ? "bold" : "normal"};
+    &:focus{
+        outline: none;
+        background: ${theme.focus};               
+    }
+    &:after{
+        content: "";
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background-size: contain;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-image: url(${arrow});
+        margin-left: 7px;
+    }
+`
+
+const StyledDialog = styled(Dialog)`
+    max-width: 500px;
+    position: relative;
+    padding: 0px;
+`
+
+const Inner = styled.fieldset`
+    display: block;
+    border: none;
+    padding: 30px; 
+    padding-bottom: 20px;     
+`
+
+const Headline = styled.h3`
+    color: ${theme.darkText};
+    font-size: 1.5em;
+    margin-bottom: 25px;
+`
+
+const Grid = styled.div`
+    @media screen and (min-width: ${theme.tablet}){
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-column-gap: 30px;
+    }
+`
+
+const CloseButton = styled.button`
+    background: none;
+    border: none;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+    &:focus{
+        outline: 3px solid ${theme.focus};
+        background: ${theme.focus};               
+    }
+`
+
+const Footer = styled.footer`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const Button = styled.button`
+    background: none;
+    border: none;
+    color: ${theme.darkText};
+    font-size: 1em;
+    font-weight: bold;
+    cursor: pointer;
+    padding: 20px 30px 30px 30px;
+    &:focus{
+        outline: 3px solid ${theme.focus};
+    }
+`
+
+const ClearButton = styled(Button)`
+    order: -1;
+`
+
 const Filter = ({
-    dialogOpen,
-    toggleDialog,
+    label,
     name,
-    active,
-    children
-}) => 
-    <>
-        <OpenerButton active={active} onClick={() => {toggleDialog(true)}} className={`${name}-opener`}>
-            {name}
-        </OpenerButton>
-        <StyledDialog
-            isOpen={dialogOpen}
-            className={`${name}-dialog`}
-            onDismiss={()=>toggleDialog(false)}
-        >
-            <CloseButton onClick={()=>toggleDialog(false)}>
-                <img src={cross} alt="Close without saving"/>
-            </CloseButton>
-            <Headline>{name}</Headline>
-            {children}
-        </StyledDialog>
-    </>
+    options
+}) => {
+
+    const [dialogOpen, toggleDialog] = useState(true)
+    const [selection, changeSelection] = useState([]) 
+
+    useEffect(()=>{
+        setSelectionFromQuery()
+    },[])
+
+    const setSelectionFromQuery = () =>{
+        if(Router.query[name]) changeSelection([].concat(Router.query[name]))
+    }
+
+    const closeWithoutSaving = () => {
+        console.log("closing without saving")
+        setSelectionFromQuery()
+        toggleDialog(false)
+    }
+
+    const handleChange = (e) => {
+        let {checked, value} = e.target
+        if(checked){
+            changeSelection([...selection, value]  )
+        } else {
+            changeSelection(selection.filter(el=> el != value))
+        }
+    }
+
+    const clearFilter = (e) => {
+        if(e) e.preventDefault()
+        changeSelection([])
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        Router.push({
+            pathname: "/recommendations",
+            query: {
+                ...Router.query,
+                [name]: selection
+            }
+        })
+        toggleDialog(false)
+    }
+
+    return(
+        <>
+            <OpenButton 
+                active={selection.length > 0} 
+                onClick={() => {toggleDialog(true)}} 
+                className={`${label}-opener`}
+            >
+                {label}
+            </OpenButton>
+            <StyledDialog
+                isOpen={dialogOpen}
+                className={`${label}-dialog`}
+                onDismiss={closeWithoutSaving}
+            >
+                <CloseButton onClick={closeWithoutSaving}>
+                    <img src={cross} alt="Close without saving"/>
+                </CloseButton>
+                <form onSubmit={handleSubmit}>
+                    <Inner>
+                        <Headline><legend>{label}</legend></Headline>
+                        <Grid>
+                            {options.map((option)=>
+                                <Checkbox 
+                                    key={option.value}
+                                    name={name} 
+                                    value={option.value} 
+                                    onChange={handleChange} 
+                                    checked={selection.includes(option.value)}
+                                >
+                                    {option.label}
+                                </Checkbox>
+                            )}
+                        </Grid>
+                    </Inner>
+                    <Footer>
+                        <Button type="submit">Apply filters</Button>
+                        <ClearButton onClick={clearFilter}>Clear</ClearButton>
+                    </Footer>
+                </form>
+            </StyledDialog>
+        </>
+    )
+}
 
 export default Filter
--- components/Filters/index.jsx
+++ components/Filters/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import theme from "../_theme"
 import LocationFilter from "./LocationFilter"
-import InterestsFilter from "./InterestsFilter"
+import Filter from "./Filter"
 
 const Outer = styled.section`
     padding: 0px 20px;
@@ -14,11 +14,76 @@ const Inner = styled.div`
     margin-right: auto;
 `
 
+const interestsOptions = [
+    {
+        value: "active",
+        label: "Staying active"
+    },
+    {
+        value: "social",
+        label: "Socialising"
+    },
+    {
+        value: "support",
+        label: "Support with daily tasks"
+    },
+    {
+        value: "learning",
+        label: "Learning new things"
+    },
+    {
+        value: "cultural",
+        label: "Culture and visiting new places"
+    }
+]
+
+const daysOptions = [
+    {
+        value: "monday",
+        label: "Monday"
+    },
+    {
+        value: "tuesday",
+        label: "Tuesday"
+    },
+    {
+        value: "wednesday",
+        label: "Wednesday"
+    },
+    {
+        value: "thursday",
+        label: "Thursday"
+    },
+    {
+        value: "friday",
+        label: "Friday"
+    },
+    {
+        value: "saturday",
+        label: "Saturday"
+    },
+    {
+        value: "sunday",
+        label: "Sunday"
+    },
+]
+
 const Filters = () =>
     <Outer>
         <Inner>
-            <LocationFilter/>
-            <InterestsFilter/>
+            {/* <LocationFilter/> */}
+
+            <Filter
+                label="Interests"
+                name="category"
+                options={interestsOptions}
+            />
+            <Filter
+                label="When you're free"
+                name="days"
+                options={daysOptions}
+            />
+
         </Inner>
     </Outer>
     
