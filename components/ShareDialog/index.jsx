250115f0ef1216b05b40ee5d9b800dcaa8db22d3
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-07 22:33:32 +0100

    share dialog looks the part

--- components/ShareDialog/index.jsx
+++ components/ShareDialog/index.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import theme from "../_theme"
 import Radio from "../Radio"
 import send from "./send.svg"
+import fetch from "isomorphic-unfetch"
 import {
     StyledDialog,
     Inner,
@@ -12,6 +13,20 @@ import {
     Button,
     CloseButton
 } from "../Filters/utils"
+
+const TextInput = styled.input`
+margin-top: 10px;
+font-size: 1em;
+border: 2px solid ${theme.darkText};
+padding: 10px;
+border-radius: 2px;
+display: block;
+width: 100%;
+&:focus{
+    outline: none;
+    box-shadow: 0 0 0 3px ${theme.focus};
+}
+`
 
 const OpenButton = styled.button`
     background: ${theme.darkText};
@@ -42,9 +57,44 @@ const OpenButton = styled.button`
     }
 `
 
-const Filter = () => {
+const Fieldset = styled.fieldset`
+    border: none;
+    padding-top: 10px;
+    margin-bottom: 20px;
+`
+
+const Alert = styled.span``
+
+const Filter = ({
+    shareableUrl,
+    singleService
+}) => {
 
     const [dialogOpen, toggleDialog] = useState(false)
+
+    const [response, setResponse] = useState(false)
+    const [recipient, changeRecipient] = useState("")
+    const [medium, changeMedium] = useState("email")
+
+    console.log(medium)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try{
+            const res = await fetch(`/api/share/${medium}`, {
+                method: "post",    
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({ 
+                    url: (shareableUrl)? window.location.host + shareableUrl : window.location.href, 
+                    email: recipient,
+                    phoneNumber: recipient
+                })
+            })
+            setResponse(res.status)
+        } catch(e){
+            setResponse("fail")
+        }
+    }
 
     return(
         <>
@@ -56,20 +106,43 @@ const Filter = () => {
             </OpenButton>
             <StyledDialog
                 isOpen={dialogOpen}
-                className="share-dialog"
                 onDismiss={() => toggleDialog(false)}
             >
                 <CloseButton onClick={() => toggleDialog(false)}>
                     <img src={cross} alt="Close without saving"/>
                 </CloseButton>
-   
-                <Inner>
-                    <Headline>Share</Headline>
-                    Test
-                </Inner>
-                <Footer>
-                    <Button type="submit">Send</Button>
-                </Footer>
+                    
+
+                <form method="post" action={`/share/${medium}`} onSubmit={handleSubmit} aria-live="polite">
+                    <Inner>
+                        <Headline>{(singleService)? "Share this service" : "Share these recommendations"}</Headline>
+                
+                        {(response === 500 || response === "fail") && <Alert>There was a problem sharing. If this continues, please try again later</Alert>}
+                        {(response === 404) && <Alert>We couldn't share to that {(medium === "sms")? "phone number" : "email"}. Please check it and try again.</Alert>}
+
+                        <Fieldset>
+                            <legend>Share by:</legend>
+                            <Radio required name="medium" onChange={e => changeMedium(e.target.value)} checked={"email" === medium} value="email">Email</Radio>
+                            <Radio required name="medium" onChange={e => changeMedium(e.target.value)} checked={"sms" === medium} value="sms">Text message</Radio>
+                        </Fieldset>
+
+                        <label htmlFor="recipient">{(medium === "sms")? "Phone number" : "Email address"} </label>
+                        <TextInput 
+                            type={(medium === "sms")? "tel" : "email"} 
+                            name="recipient"
+                            id="recipient"
+                            required
+                            value={recipient}
+                            onChange={(e)=>{
+                                changeRecipient(e.target.value)
+                            }}
+                        />
+
+                    </Inner>
+                    <Footer>
+                        <Button type="submit">Send {(medium === "sms")? "message" : "email"}</Button>
+                    </Footer>
+                </form>
 
             </StyledDialog>
         </>
