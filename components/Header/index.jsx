e708c6185a28a2feae0f8f78aaf752fd3d777f51
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-09 17:13:46 +0100

    fix merge errors

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,60 +2,6 @@ import React from "react"
 import Link from "next/link"
 import styled from "styled-components"
 import logo from "./logo.svg"
-<<<<<<< HEAD
-import invertedLogo from "./logo-inverted.svg"
-
-const Header = ({inverted}) => {
-    let menuItems = [
-        {
-            href: "#",
-            text: "Information and advice"
-        },
-        {
-            href: "/explore-your-needs",
-            text: "Things to do"
-        },
-        {
-            href: "#",
-            text: "Log in"
-        },
-        {
-            href: "#",
-            text: "Sign up"
-        },
-    ]
-    return(
-        <header className={(inverted)? "site-header site-header--inverted": "site-header"}>
-            <div className="site-header__inner container">
-                <Link href="/">
-                    <a>
-                        <img className="site-header__logo" src={(inverted)? invertedLogo : logo} alt="Buckinghamshire County Council"/>
-                    </a>
-                </Link>
-
-                <MobileMenu menuItems={menuItems} inverted={inverted}/>
-
-                <nav className="site-header__navigation" role="navigation">
-                    <SearchForm/>
-                    <ul className="site-header__menu site-menu" >
-                        {(menuItems.map((menuItem, i)=>
-                            <MenuItem href={menuItem.href} text={menuItem.text} key={i}/>    
-                        ))}
-                    </ul>
-                </nav>
-
-            </div>
-        </header>
-    )
-}
-
-
-Header.propTypes = {
-    inverted: PropTypes.bool
-}
-
-export default Header
-=======
 import eyeglass from "./eyeglass.svg"
 import theme from "../_theme"
 
@@ -209,4 +155,3 @@ const SiteHeader = () =>
     </Outer>
 
 export default SiteHeader
->>>>>>> next-gen
