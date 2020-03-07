887c5d473024d22ae4942e4f150d8928cdcb40a1
Jaye Hackett <joshua.hackett93@gmail.com>
2020-03-07 12:57:26 +0000

    Update print.js

--- controllers/print.js
+++ controllers/print.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
             format: "Letter",
             orientation: "portrait",
             border: "1in",
-            zoom: "0.5"
+            zoom: 0.5
         }).toStream((err, stream)=>{
             res.setHeader('Content-type', 'application/pdf')
             stream.pipe(res)
